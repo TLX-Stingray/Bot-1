dcdd723237460a771814f73e3ef1b301988a5df0
Lourens Ros <lourensros@gmail.com>
2018-06-22 16:37:31 +0200

    Changed Help Text to accomodate Mention and new commands (serverinfo)

--- src/main/java/Discord/bot/Ref.java
+++ src/main/java/Discord/bot/Ref.java
@@ -10,13 +10,17 @@ public class Ref
         public static final Color userCL = Color.ORANGE;
         public static final Color botCL = Color.BLUE;
 
-        public static final String cmds = "``` Bot Commands and parameter with description '{}' is optional parameters and '()' is required (leave out the brackets in both cases). Remember to add the prefix if you forgot\n" +
+        public static final String cmds = "``` Bot Commands and parameter with description '{}' is optional parameters and " +
+                "'()' is required (leave out the brackets in both cases). " +
+                "Remember to add the prefix if you forgot" +
+                "(You can Also Mention the bot for commands \n" +
                 "Help - Duh, the one you are watching. \n" +
                 "Userinfo @{User} - Returns info of user using command or if another user is mentioned it will give info about user \n" +
                 "Botinfo - Shows info of bot. \n" +
+                "Serverinfo - Shows info About the server \n" +
                 "Channelinfo - shows info of the current text channel \n" +
                 "Clear (amount) - clears the specified amount of message history from the current channel \n" +
-                "Emptychannel - Delete all the accesible messages in the channel```";
+                "Emptychannel - Delete all the accesible messages in the channel ```";
 
         public static boolean isInteger(String s) {
                 try {
